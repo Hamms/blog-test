4357b37a8d88c3ddb03de2b31689d27c131c6d0b
Elijah Hamovitz <elijah@node.emu.uoregon.edu>
2011-03-09 13:05:48 -0800

    adding factory3 example

---
+++ articles/messing-with-scope/factory3.js
@@ -0,0 +1,27 @@
+//definition
+function factory(x){
+  var that = {}
+  var f1 = that.f1 = function(){
+    console.log(x)
+  },
+
+  var f2 = that.f2 = function(y){
+    f1();
+    console.log(y)
+  }
+  return that;
+}
+
+var printer = factory('Howdy');
+
+printer.f2('Pardner');
+
+//redefinition
+printer.f1 = function(){
+  console.log('Bugger off');
+}
+
+printer.f2('Pardner');
+
+
+
